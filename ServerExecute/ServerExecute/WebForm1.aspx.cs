1d7f23d1028af7f64f6180ad89f08592f33aa715
pradeep <pradeep.budhathoki1234@gmail.com>
2017-09-18 12:40:35 +0545

    another commit

---
+++ ServerExecute/ServerExecute/WebForm1.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServerExecute
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnTransfer_Click(object sender, EventArgs e)
+        {
+            Server.Transfer("~/WebForm2.aspx");
+            lblStatus.Text = "process terminated in webform1";
+        }
+    }
+}
---
+++ ServerExecute/ServerExecute/WebForm2.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ServerExecute
+{
+    public partial class WebForm2 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            System.Collections.Specialized.NameValueCollection previousCollection = Request.Form;
+            lblName.Text = previousCollection["txtName"];
+            lblEmail.Text = previousCollection["txtEmail"];
+
+            
+        }
+    }
+}
